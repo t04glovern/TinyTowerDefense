fd731d17be175c1fa3f16cc672b35ad9001ade1d
Nathan Glover <nathan@glovers.id.au>
2024-01-05 21:13:02 +0800

    63. Polish and Balance

--- Base/base.gd
+++ Base/base.gd
@@ -18,7 +18,6 @@ var current_health: int:
 
 func _ready() -> void:
     current_health = max_health
-    # Engine.time_scale = 5
 
 
 func take_damage() -> void:
--- Turret/projectile.gd
+++ Turret/projectile.gd
@@ -5,6 +5,7 @@ var direction: Vector3 = Vector3.FORWARD
 
 
 @export var speed: float = 30.0
+@export var damage: int = 25
 
 
 func _physics_process(delta: float) -> void:
@@ -17,5 +18,5 @@ func _on_timer_timeout() -> void:
 
 func _on_area_entered(area: Area3D) -> void:
 	if area.is_in_group("enemy_area"):
-		area.get_parent().current_health -= 25
+		area.get_parent().current_health -= damage
 		queue_free()
