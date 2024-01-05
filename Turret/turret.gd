64a03af86a915ae1e2383e0876cbacc4966abcd7
Nathan Glover <nathan@glovers.id.au>
2024-01-05 17:29:04 +0800

    48. Aiming the Turrets

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -3,10 +3,18 @@ extends Node3D
 
 @export var projectile: PackedScene
 
+var enemy_path: Path3D
+
 @onready var turret_top: Node3D = $TurretBase/TurretTop
 
 
-func _on_timer_timeout():
-    var shot = projectile.instantiate()
-    add_child(shot)
-    shot.global_position = turret_top.global_position
+func _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
+
+
+func _on_timer_timeout() -> void:
+	var shot = projectile.instantiate()
+	add_child(shot)
+	shot.global_position = turret_top.global_position
+	shot.direction = turret_top.global_transform.basis.z
