3fd100c9d9b9cfdc6acdfd3f205f25665d2d1ec2
Nathan Glover <nathan@glovers.id.au>
2024-01-05 18:52:23 +0800

    50. For Loops and Targeting

--- RayPickerCamera/ray_picker_camera.gd
+++ RayPickerCamera/ray_picker_camera.gd
@@ -9,7 +9,7 @@ extends Camera3D
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta: float) -> void:
 	var mouse_position: Vector2 = get_viewport().get_mouse_position()
 	ray_cast_3d.target_position = project_local_ray_normal(mouse_position) * clickable_range 
 	ray_cast_3d.force_raycast_update()
--- Turret/turret.gd
+++ Turret/turret.gd
@@ -2,19 +2,36 @@ extends Node3D
 
 
 @export var projectile: PackedScene
+@export var turret_range: float = 10.0
 
 var enemy_path: Path3D
+var target: PathFollow3D
 
 @onready var turret_top: Node3D = $TurretBase/TurretTop
 
 
-func _physics_process(delta: float) -> void:
-	var enemy = enemy_path.get_children().back()
-	look_at(enemy.global_position, Vector3.UP, true)
+func _physics_process(_delta: float) -> void:
+	target = find_best_target()
+	if target:
+		look_at(target.global_position, Vector3.UP, true)
 
 
 func _on_timer_timeout() -> void:
-	var shot = projectile.instantiate()
-	add_child(shot)
-	shot.global_position = turret_top.global_position
-	shot.direction = turret_top.global_transform.basis.z
+	if target:
+		var shot = projectile.instantiate()
+		add_child(shot)
+		shot.global_position = turret_top.global_position
+		shot.direction = turret_top.global_transform.basis.z
+
+
+func find_best_target() -> PathFollow3D:
+	var best_target = null
+	var best_progress = 0
+	for enemy in enemy_path.get_children():
+		if enemy is PathFollow3D:
+			if enemy.progress > best_progress:
+				var distance = global_position.distance_to(enemy.global_position)
+				if distance < turret_range:
+					best_target = enemy
+					best_progress = enemy.progress
+	return best_target
