ce831f853a235138385748c40bbe5a828a90ba72
Nathan Glover <nathan@glovers.id.au>
2024-01-05 15:56:16 +0800

    40. Text, Strings and Colors

--- Base/base.gd
+++ Base/base.gd
@@ -7,7 +7,10 @@ var current_health: int:
     set(health_in):
         current_health = health_in
         print("Health was changed")
-        label_3d.text = str(current_health)
+        label_3d.text = str(current_health) + "/" + str(max_health)
+        var red: Color = Color.RED
+        var white: Color = Color.WHITE
+        label_3d.modulate = red.lerp(white, float(current_health) / float(max_health))
         if current_health < 1:
             get_tree().reload_current_scene()
 
