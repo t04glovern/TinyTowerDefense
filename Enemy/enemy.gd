91a3e8151692e5d940b0a6574d907bb223b7092e
Nathan Glover <nathan@glovers.id.au>
2024-01-05 19:05:43 +0800

    51. Introducing Animations

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -5,18 +5,21 @@ extends PathFollow3D
 
 var current_health: int:
 	set(health_in):
+		if health_in < current_health:
+			animation_player.play("TakeDamage")
 		current_health = health_in
 		if current_health < 1:
 			queue_free()
 
 @onready var base = get_tree().get_first_node_in_group("base")
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
 	current_health = max_health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float) -> void:
 	progress += delta * speed
 	if progress_ratio == 1.0:
 		base.take_damage()
