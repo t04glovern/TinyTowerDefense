ecac7d6c184e7961684cfd6c5483ebb2ad60e36e
Nathan Glover <nathan@glovers.id.au>
2024-01-05 18:25:08 +0800

    49. Damaging Enemies

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,9 +1,19 @@
 extends PathFollow3D
 
 @export var speed: float = 5.0
+@export var max_health: int = 50
+
+var current_health: int:
+	set(health_in):
+		current_health = health_in
+		if current_health < 1:
+			queue_free()
 
 @onready var base = get_tree().get_first_node_in_group("base")
 
+
+func _ready() -> void:
+	current_health = max_health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
